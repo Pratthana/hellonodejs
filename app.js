83d62b751e9cff5fe72069eda1d13c061ddb5f92
pratthana <pratthana07052@gmail.com>
2020-01-29 18:23:46 +0700

    Init

---
+++ app.js
@@ -0,0 +1,36 @@
+const say = require('say');
+// say.speak("Hello", 'Alex', 0.3)
+
+//nodejs จะไม่รอตัวที่ทำงานช้า มันจะข้ามไปทำตัวอื่นก่อน
+const axios = require('axios');
+const apiURL = "api.openweathermap.org/data/2.5/weather?q=";
+const city = "Phuket";
+const appID = "7430b7e61b2cbe834292e06e2cf07aa2";
+const url = `http://${apiURL}${city}&appid=${appID}`;
+console.log(url);
+
+sayWeather();
+
+async function sayWeather(){
+    const response = await axios.get(url);
+    console.log(response.data);
+    say.speak(`${city} weather is ${response.data.weather[0].description}`);
+    console.log("Finished");
+}
+
+//ระบบ promise เพื่อเช็คการทำงาน
+// axios.get(url)
+//     .then(function (response) {
+//         // handle success
+//         console.log(response.data);
+//         say.speak(`${city} weather is ${response.data.weather[0].description}`);
+//         console.log("Finished");
+//     })
+//     .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//     })
+
+
+
+
